79aa6c56fb20d6e28039935a0b7b70d175b1a80b
PiyushG-dev <pjgpiyush@gmail.com>
2024-02-27 20:23:55 +0530

    bug fixes

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,6 +4,8 @@ import img1 from "../../images/karan.jpg";
 import img2 from "../../images/dyou.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 const Work = () => {
   return (
@@ -13,11 +15,39 @@ const Work = () => {
         <p>From our team of creatives.</p>
       </div>
       <div className={styles.projects_container}>
-        <a href="" className={styles.box}>
-          <img src={img1} alt="" />
+        <a
+          href="https://www.youtube.com/watch?v=I8QWBFDMT5A"
+          className={styles.box}
+        >
+          <img src={img1} alt="cover" />
+          <div className={styles.cta}>
+            <p>Tumarei hahi</p>
+            <div className={styles.links}>
+              <a href="https://open.spotify.com/track/14CR9CwvpIrfpW9YNoOnNs?si=b5ddcb3e55234188">
+                <FontAwesomeIcon icon={faSpotify} />
+              </a>
+              <a href="https://www.youtube.com/watch?v=I8QWBFDMT5A">
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+            </div>
+          </div>
         </a>
-        <a href="" className={styles.box}>
-          <img src={img2} alt="" />
+        <a
+          href="https://www.youtube.com/watch?v=-6HoMtV-KGk"
+          className={styles.box}
+        >
+          <img src={img2} alt="cover" />
+          <div className={styles.cta}>
+            <p>Dithokot xopun</p>
+            <div className={styles.links}>
+              <a href="https://open.spotify.com/track/4DzAfEwZKtUIOG4wJSbGhq?si=dc459278d5464f8f">
+                <FontAwesomeIcon icon={faSpotify} />
+              </a>
+              <a href="https://www.youtube.com/watch?v=-6HoMtV-KGk">
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+            </div>
+          </div>
         </a>
       </div>
       <div className={styles.socials}>
