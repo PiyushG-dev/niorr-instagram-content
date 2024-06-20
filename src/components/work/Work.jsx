c560f5fd9d601c6ffe96f5afcf36b99fa4598328
Piyush Goswami <pjgpiyush@gmail.com>
2024-06-20 18:40:12 +0530

    new song added

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -3,7 +3,7 @@ import styles from "./Work.module.css";
 import img1 from "../../images/karan.jpg";
 import img2 from "../../images/dyou.jpg";
 import img3 from "../../images/ac.jpg";
-import img4 from "../../images/cs.jpg";
+import img4 from "../../images/dyou2.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
@@ -13,11 +13,28 @@ const Work = () => {
   return (
     <section className={styles.wrapper}>
       <div className={styles.heading}>
-        <h2>Featured exclusives</h2>
+        <h2>Niorr Exclusives</h2>
         <p>From our team of creatives.</p>
       </div>
       <div className={styles.projects}>
         <div className={styles.projects_container}>
+          <a
+            href="https://www.youtube.com/watch?v=zpKDRVdEOvc"
+            className={styles.box}
+          >
+            <img src={img4} alt="cover" />
+            <div className={styles.cta}>
+              <p>Kio nubuja / Timi nai bhanana</p>
+              <div className={styles.links}>
+                <a href="https://open.spotify.com/track/69WzSpFTnNY5be9KSve3Pv?si=860195350cf346ee">
+                  <FontAwesomeIcon icon={faSpotify} />
+                </a>
+                <a href="https://www.youtube.com/watch?v=zpKDRVdEOvc">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+              </div>
+            </div>
+          </a>
           <a
             href="https://www.youtube.com/watch?v=I8QWBFDMT5A"
             className={styles.box}
@@ -30,23 +47,6 @@ const Work = () => {
                   <FontAwesomeIcon icon={faSpotify} />
                 </a>
                 <a href="https://www.youtube.com/watch?v=I8QWBFDMT5A">
-                  <FontAwesomeIcon icon={faYoutube} />
-                </a>
-              </div>
-            </div>
-          </a>
-          <a
-            href="https://www.youtube.com/watch?v=-6HoMtV-KGk"
-            className={styles.box}
-          >
-            <img src={img2} alt="cover" />
-            <div className={styles.cta}>
-              <p>Dithokot xopun</p>
-              <div className={styles.links}>
-                <a href="https://open.spotify.com/track/4DzAfEwZKtUIOG4wJSbGhq?si=dc459278d5464f8f">
-                  <FontAwesomeIcon icon={faSpotify} />
-                </a>
-                <a href="https://www.youtube.com/watch?v=-6HoMtV-KGk">
                   <FontAwesomeIcon icon={faYoutube} />
                 </a>
               </div>
@@ -71,8 +71,22 @@ const Work = () => {
               </div>
             </div>
           </a>
-          <a href="https://www.instagram.com/niorr.in/" className={styles.box}>
-            <img src={img4} alt="cover" />
+          <a
+            href="https://www.youtube.com/watch?v=-6HoMtV-KGk"
+            className={styles.box}
+          >
+            <img src={img2} alt="cover" />
+            <div className={styles.cta}>
+              <p>Dithokot xopun</p>
+              <div className={styles.links}>
+                <a href="https://open.spotify.com/track/4DzAfEwZKtUIOG4wJSbGhq?si=dc459278d5464f8f">
+                  <FontAwesomeIcon icon={faSpotify} />
+                </a>
+                <a href="https://www.youtube.com/watch?v=-6HoMtV-KGk">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+              </div>
+            </div>
           </a>
         </div>
       </div>
